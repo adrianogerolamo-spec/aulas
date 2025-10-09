c6239631e54e7623b275b25900a6e109e7ebc1ce
adrianogerolamo-spec <adriano.gerolamo@usp.br>
2025-10-09 15:21:25 -0300

    Update on python_aula_6.py

--- python_aula_6.py
+++ python_aula_6.py
@@ -1,16 +1,21 @@
-
-
 #ex5
-genes = {}
-count = 0
+import sys
 
-with open("Python_06.fastq", "r") as file:
-    while True:
-        count += 1        
-        for line in file:
-            if count%2 ==0:
-                line = line.rstrip()
-                id, seq = line.split()
-                genes[id] = seq
+file = sys.argv[1]
+fastaDict = {}
 
-print(genes)
+#seq = ''
+#identificador = ''
+
+with open(file, 'rt') as f:
+    for line in f:
+        line = line.rstrip() #remove o char invisivel
+        if line.startswith("@"):
+            identificador = line.split(" ")[0].replace('@','')
+            if identificador not in fastaDict.keys():
+                fastaDict[identificador] = ''
+        else:
+            fastaDict[identificador] = fastaDict[identificador] + line
+for id in fastaDict.keys():
+    print(f'id: {id}')
+    print(f'fasta: {fastaDict[id]}')
