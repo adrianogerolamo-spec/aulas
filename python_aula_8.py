b9e8b73276d3a910df1227041e9a35ebc45c816d
adrianogerolamo-spec <adriano.gerolamo@usp.br>
2025-10-30 14:46:24 -0300

    add error alula 9

--- python_aula_8.py
+++ python_aula_8.py
@@ -1,5 +1,5 @@
 #ex1
-
+'''
 with open("Python_08.fasta", "rt") as file:
     #print(len(file.read()))
     #text = file.read()
@@ -14,4 +14,19 @@ with open("Python_08.fasta", "rt") as file:
         print(f"gene {gene[0]}:")
         for char in "ATCG":
             print(f"{char}: "+str(gene[1].count(char)))
-        
+'''
+import sys
+
+file = ''
+try:
+  file = sys.argv[1]
+  print("User provided file name:" , file)
+  if not file.endswith('.fasta'):
+    raise ValueError("Not a FASTA file")
+  FASTA = open(file, "r")
+  for line in FASTA:
+    print(line)
+except IndexError:
+  print("Please provide a file name")
+except IOError as ex:
+  print("Can't find file:" , file , ': ' , ex.strerror  )        
